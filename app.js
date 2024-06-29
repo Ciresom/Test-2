b8d673f07341c30de88ca84c6ee98e8887effcdd
Ciresom <166697177+Ciresom@users.noreply.github.com>
2024-06-29 20:24:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 const cardArray = [
   {
     name:'cheeseburger',
-    img:'IMG_3489.png',
+    img:'IMG_3489.png'
   },
     name:'fries',
-    img:'IMG_3490.png',
+    img:'IMG_3490.png'
   },
   {
     name:'hotdog',
-    img:'IMG_3491.png',
+    img:'IMG_3491.png'
   },
   {
     name:'ice-cream',
-    img:'IMG_3492.png',
+    img:'IMG_3492.png'
   },
   {
     name:'milk-shake',
-    img:'IMG_3493.png',
+    img:'IMG_3493.png'
   },
   {
     name:'pizza',
-    img:'IMG_3494.png',
+    img:'IMG_3494.png'
   },
 ]
   
